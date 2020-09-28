56a6464af13c27a3108448d6dd34ba773ea5b397
boilerb83 <boilerb83@gmail.com>
2020-09-28 18:47:10 -0400

    finalized code

--- Tiger/Animal.cs
+++ Tiger/Animal.cs
@@ -7,30 +7,20 @@ using System.Threading.Tasks;
 
 namespace Tiger
 {
-
     public class Animal
     {
-
-        //Public List<string> listOfHints { get; set; }
-
-        //listOfHints.Add(1) = "It can run fast";
-        //listOfHints.Add(2) = "It has a long tail";
-        //listOfHints.Add(3) = "It has orange fur";
-        //listOfHints.Add(4)="It has dark vertical stripes.";
-        //listOfHints.Add(5)="It can run > 35 mph.";
-        //"It has fur gives high camouglage that is orange and black.",
-
-        public int Hints { get; set; }
-
-        public string[] listOfHints { get; set; } = new string[] {"It can run fast", "It lives in the sahara", "it's a tiger"};
-
         public Animal() { }
-    }
 
-    public class Elephant
-    { 
-        
-    
+        public string[] listOfHints { get; set; } = new string[] 
+        {
+            "HINT:: IT CAN RUN FAST.", 
+            "HINT:: IT LIVES IN THE SAHARA", 
+            "HINT:: IT HAS A LONG TAIL", 
+            "HINT:: IT HAS ORANGE FUR", 
+            "HINT:: IT HAS DARK VERTICAL STRIPES", 
+            "HINT:: IT CAN RUN GREATER THAN 35 MPH.",
+            "HINT:: IT HAS FUR THAT GIVES HIGH CAMOFLAUGE THAT IS ORANGE AND BLACK."
+        }; 
     }
 }
 
